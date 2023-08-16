e2859ea6093bdac1bd61b0f448b1dde051743cdc
Daffa SH <daffa.haramaini@dayainspirasi.id>
2023-08-16 23:13:38 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
